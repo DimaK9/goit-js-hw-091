e5853d1beea29932e0026a698453b878aca5369a
DimaK9 <103372116+DimaK9@users.noreply.github.com>
2022-11-13 19:10:30 +0000

    Deploying to gh-pages from @ DimaK9/goit-js-hw-091@f4885a7447c7e36198f2fed398681474a9ed139f 🚀

---
+++ 01-color-switcher.74ce1228.js
@@ -0,0 +1,2 @@
+!function(){var t={body:document.querySelector("body"),btnStart:document.querySelector(".js-start"),btnStop:document.querySelector(".js-stop")},n=null;t.btnStart.addEventListener("click",(function(){n=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.btnStart.disabled=!0})),t.btnStop.addEventListener("click",(function(){clearInterval(n),t.startBtn.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.74ce1228.js.map
