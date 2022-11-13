f4885a7447c7e36198f2fed398681474a9ed139f
DimaK9 <103372116+DimaK9@users.noreply.github.com>
2022-11-13 21:10:03 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
 
+const refs = {
+    body: document.querySelector('body'),
+    btnStart: document.querySelector('.js-start'),
+    btnStop: document.querySelector('.js-stop'),
+};
+
+
+let timerId = null; 
+
+refs.btnStart.addEventListener('click', ontBtnStartClick);
+refs.btnStop.addEventListener('click', onBtnStopClick);
+
+function ontBtnStartClick() {
+    timerId = setInterval(() =>{
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    refs.btnStart.disabled = true;
+}
+
+function onBtnStopClick() {
+    clearInterval(timerId);
+    refs.startBtn.disabled = false;
+}
